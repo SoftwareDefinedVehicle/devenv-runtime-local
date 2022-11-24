f5736489f0a6e2dba37834b068bc737e13fd4414
Dennis Meister <dennis.meister@bosch.com>
2022-11-24 13:43:46 +0100

    Test scripts
    Signed-off-by: Dennis Meister <dennis.meister@bosch.com>

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 echo "Hello, Runtime Local!"
-printenv
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-${SCRIPT_DIR}/ensure-dapr.sh
+if [ ! -d ../logs ]
+  then mkdir ../logs
+fi
+${SCRIPT_DIR}/ensure-dapr.sh >> ../logs/ensure-dapr.log 2>> ../logs/ensure-dapr.log
+${SCRIPT_DIR}/run-feedercan.sh >> ../logs/run-feedercan.log 2>&1
+${SCRIPT_DIR}/run-mosquitto.sh >> ../logs/run-mosquitto.log 2>&1
+${SCRIPT_DIR}/run-vehicledatabroker.sh >> ../logs/run-vehicledatabroker.log 2>&1
+${SCRIPT_DIR}/run-vehicleservices.sh >> ../logs/run-vehicleservices.log 2>&1
