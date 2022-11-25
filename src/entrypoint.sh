5e6a00748968527f7ea63c44814dc33ca4965bae
Dennis Meister <dennis.meister@bosch.com>
2022-11-25 13:01:46 +0100

    Fix entrypoint and manifest
    Signed-off-by: Dennis Meister <dennis.meister@bosch.com>

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -8,7 +8,7 @@ if [ ! -d $LOG_DIR ]
     then mkdir -p $LOG_DIR
 fi
 
-${SCRIPT_DIR}/ensure-dapr.sh 2>&1 | awk '{print "runtime-local, ensure-dapr.sh, " $0}' >> ${LOG_DIR}/runtime-local.log &
+${SCRIPT_DIR}/ensure-dapr.sh 2>&1 | awk '{print "runtime-local, ensure-dapr.sh, " $0}' >> ${LOG_DIR}/runtime-local.log
 ${SCRIPT_DIR}/run-feedercan.sh 2>&1 | awk '{print "runtime-local, run-feedercan.sh, " $0}' >> ${LOG_DIR}/runtime-local.log &
 ${SCRIPT_DIR}/run-mosquitto.sh 2>&1 | awk '{print "runtime-local, run-mosquitto.sh, " $0}' >> ${LOG_DIR}/runtime-local.log &
 ${SCRIPT_DIR}/run-vehicledatabroker.sh 2>&1 | awk '{print "runtime-local, run-vehicledatabroker.sh, " $0}' >> ${LOG_DIR}/runtime-local.log &
